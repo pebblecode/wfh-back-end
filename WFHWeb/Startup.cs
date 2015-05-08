9d684f2cb848a62c58bc11d3db5ec758bcb2e699
Simon Dickson <simonhdickson@gmail.com>
2015-05-08 15:08:36 +0100

    Fix signalr

---
+++ WFHWeb/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.SignalR;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WFHWeb
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var signalrConfig = new HubConfiguration();
+
+            app.MapSignalR(signalrConfig);
+        }
+    }
+}
