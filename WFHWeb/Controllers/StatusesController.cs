9e1a9954c8d65d7ad0d74e5acee9bc4e1c84115e
Simon Dickson <simonhdickson@gmail.com>
2015-05-08 13:28:00 +0100

    Fix build

--- WFHWeb/Controllers/StatusesController.cs
+++ WFHWeb/Controllers/StatusesController.cs
@@ -7,6 +7,7 @@ namespace WFHWeb.Controllers
     using Newtonsoft.Json.Linq;
     using System;
     using System.Collections.Generic;
+    using System.Configuration;
     using System.Linq;
     using System.Net.Http;
     using System.Web;
