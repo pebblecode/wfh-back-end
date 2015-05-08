1021263402579c724e2f01269ca13f84c3c08749
Joseph Jeganathan <josephjeganathan@gmail.com>
2015-05-08 14:36:30 +0100

    Enabling CORS

--- WFHWeb/Global.asax.cs
+++ WFHWeb/Global.asax.cs
@@ -1,20 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Http;
-using System.Web.Routing;
 
 namespace WFHWeb
 {
-    using WFHWeb.App_Start;
-
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
-            GlobalConfiguration.Configure(GlobalConfig.ConfigJson);
         }
     }
 }
