f6804f918cba4e7dfa8080f2beb710e387f1112c
Simon Dickson <simonhdickson@gmail.com>
2015-05-08 16:28:54 +0100

    Go go go

--- WFHWeb/Startup.cs
+++ WFHWeb/Startup.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin;
 using Microsoft.Owin.Cors;
 using Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using WFHWeb;
 
+[assembly: OwinStartup(typeof(Startup))]
 namespace WFHWeb
 {
     public class Startup
