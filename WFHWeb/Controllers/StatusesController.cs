58967f80a7d536c23704e280953fb60fb1bc9781
Simon Dickson <simonhdickson@gmail.com>
2015-05-08 10:34:20 +0100

    Add statuses

--- WFHWeb/Controllers/StatusesController.cs
+++ WFHWeb/Controllers/StatusesController.cs
@@ -4,10 +4,18 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Data.SQLite;
 
 namespace WFHWeb.Controllers
 {
+    [RoutePrefix("/api/statuses")]
     public class StatusesController : ApiController
     {
+        [HttpPost]
+        [Route("{status}")]
+        public void SetStatus()
+        {
+            
+        }
     }
 }
