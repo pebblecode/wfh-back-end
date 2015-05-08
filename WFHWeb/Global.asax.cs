ece755f2e54e2fa699431eec86265d3747ba1372
Martin Schinz <martin.schinz@bedegaming.com>
2015-05-08 14:45:39 +0100

    enable cors

--- WFHWeb/Global.asax.cs
+++ WFHWeb/Global.asax.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace WFHWeb
 {
@@ -15,6 +11,8 @@ namespace WFHWeb
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
             GlobalConfiguration.Configure(GlobalConfig.ConfigJson);
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            GlobalConfiguration.Configuration.EnableCors(cors);
         }
     }
 }
