ce48dec90dea1dbf201a39c01c0b2b27939740a0
Simon Dickson <simonhdickson@gmail.com>
2015-05-08 12:29:52 +0100

    Add slack support

--- WFHWeb/Controllers/StatusesController.cs
+++ WFHWeb/Controllers/StatusesController.cs
@@ -3,9 +3,10 @@ using System.Web.Http;
 
 namespace WFHWeb.Controllers
 {
+    using System;
     using System.Linq;
+    using System.Net.Http;
     using System.Web;
-
     using WFHWeb.DataModels;
     using WFHWeb.Models;
     using WFHWeb.Services;
@@ -61,18 +62,17 @@ namespace WFHWeb.Controllers
         [Route("Slack")]
         public async Task<IHttpActionResult> SetStatusFromSlack()
         {
-            
+            // this is simon's token :)
+            var token = "xoxp-2165237499-2607133545-4790338792-f5ac10";
             var foo = await this.Request.Content.ReadAsStringAsync();
             var slackData = HttpUtility.ParseQueryString(foo);
-            //Get User Id
             var userid = slackData["user_id"];
-            //Get Slack info
 
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri("https://slack.com/api/");
+            var response = await httpClient.GetAsync(string.Format("users.info?token={0}&&user={1}", token, userid));
 
-            //Get Status
-            //Post Status
-            //Return 
-            return Ok(foo);
+            return Ok(response);
         }
     }
 }
