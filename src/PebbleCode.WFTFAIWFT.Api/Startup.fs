b9bb62ff0e7e172edf5dab5731c63adf01fa9a17
Martin Schinz <martin.schinz@bedegaming.com>
2015-05-25 09:47:25 +0100

    centralize and share autofac

--- src/PebbleCode.WFTFAIWFT.Api/Startup.fs
+++ src/PebbleCode.WFTFAIWFT.Api/Startup.fs
@@ -1,4 +1,4 @@
-﻿namespace PebbleCode.WTFAIWFT.Api.NotificationHub
+﻿namespace PebbleCode.WTFAIWFT.Api
 
 open FSharp.Interop.Dynamic
 open Microsoft.AspNet.SignalR
@@ -10,57 +10,76 @@ open System.Web.Http
 open Newtonsoft.Json.Converters
 open Autofac
 open Autofac.Integration.WebApi
+open Autofac.Integration.SignalR
 open System.Reflection
 open Microsoft.Owin.Host
+open System.Configuration
 
-type HttpRoute = {
-    controller : string
-    statusType : RouteParameter }
+type HttpRoute =
+    {   controller : string
+        statusType : RouteParameter }
+
+type WfhConfig =
+    {   RootFolder : string } 
+with
+    static member readFromConfigurationManager () =
+        let rootFolder = ConfigurationManager.AppSettings.["datafolder"]
+        {   RootFolder = rootFolder}
+
+module Config =
+    let configureContainerBuilder (httpConfiguration, hubConfiguration) =
+        let builder = ContainerBuilder()
+        builder.Register(fun context ->
+            WfhConfig.readFromConfigurationManager ()).AsSelf().SingleInstance() |> ignore;
+        let createRepo rootFolder =
+            let hub = GlobalHost.ConnectionManager.GetHubContext<WfhHub, NotifyWorkFromHome>()
+            let publish eve = 
+                match eve with
+                | StatusChanged sc -> hub.Clients.All.Update sc
+            WorkerStatusAggregateRepository.createFilesystemRepository (rootFolder, publish)
+        builder.Register(fun ctx ->
+            let config = ctx.Resolve<WfhConfig>()
+            createRepo config.RootFolder).AsSelf() |> ignore
+        builder.RegisterApiControllers(Assembly.GetExecutingAssembly()) |> ignore
+        builder.RegisterWebApiFilterProvider(httpConfiguration)
+        builder.RegisterHubs(Assembly.GetExecutingAssembly()) |> ignore
+        builder
+
 
 type Startup () =
 
-    static member RegisterWebApi(config: HttpConfiguration) =
+    static member RegisterWebApi(httpConfiguration: HttpConfiguration) =
         // Configure routing
-        config.MapHttpAttributeRoutes()
-        config.Routes.MapHttpRoute(
+        httpConfiguration.MapHttpAttributeRoutes()
+        httpConfiguration.Routes.MapHttpRoute(
             "DefaultApi", // Route name
             "api/{controller}/{statusType}", // URL with parameters
             { controller = "{controller}"; statusType = RouteParameter.Optional } // Parameter defaults
         ) |> ignore
 
         // Configure serialization
-        config.Formatters.XmlFormatter.UseXmlSerializer <- false
-        config.Formatters.JsonFormatter.SerializerSettings.ContractResolver <- Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver()
-        config.Formatters.JsonFormatter.SerializerSettings.Converters.Add(StringEnumConverter())
+        httpConfiguration.Formatters.XmlFormatter.UseXmlSerializer <- false
+        httpConfiguration.Formatters.JsonFormatter.SerializerSettings.ContractResolver <- Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver()
+        httpConfiguration.Formatters.JsonFormatter.SerializerSettings.Converters.Add(StringEnumConverter())
 
     member this.Configuration (app:IAppBuilder) =
 
-        let rootFolder = @"C:\data"
-        GlobalHost.DependencyResolver.Register(typeof<WfhHub>, fun () -> (new WfhHub(rootFolder) :> obj)) |> ignore
+        let httpConfiguration = new HttpConfiguration()
+        let hubConfiguration = new HubConfiguration()
+        let builder = Config.configureContainerBuilder (httpConfiguration, hubConfiguration)
+        let container = builder.Build()
+        httpConfiguration.DependencyResolver <-  new AutofacWebApiDependencyResolver(container)
+        Startup.RegisterWebApi(httpConfiguration)
+        hubConfiguration.Resolver <- new AutofacDependencyResolver(container)
+
+        app.UseAutofacMiddleware(container) |> ignore
         app.Map(
             "/signalr",
             (fun appBuilder ->
             appBuilder.UseCors(CorsOptions.AllowAll) |> ignore
             appBuilder.RunSignalR())
             ) |> ignore
-
-        let createRepo _ =
-            let hub = GlobalHost.ConnectionManager.GetHubContext<WfhHub, NotifyWorkFromHome>()
-            let publish eve = 
-                match eve with
-                | StatusChanged sc -> hub.Clients.All.Update sc
-            WorkerStatusAggregateRepository.createFilesystemRepository (rootFolder, publish)
-
-        let builder = new ContainerBuilder()
-        let config = new HttpConfiguration()
-        builder.RegisterApiControllers(Assembly.GetExecutingAssembly()) |> ignore
-        builder.RegisterWebApiFilterProvider(config);
-        builder.Register(createRepo).AsSelf() |> ignore
-        let container = builder.Build()
-        config.DependencyResolver <-  new AutofacWebApiDependencyResolver(container)
-        Startup.RegisterWebApi(config)
-        app.UseWebApi(config) |> ignore
-
+        app.UseWebApi(httpConfiguration) |> ignore
 
 [<assembly: OwinStartup(typeof<Startup>)>]
 do()
